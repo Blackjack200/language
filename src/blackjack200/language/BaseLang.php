f1e7c9d45e5d0becb45aac994b2b0e9f2864bf99
Blackjack200 <lx826444126@gmail.com>
2021-07-05 18:50:25 +0800

    implement

--- src/blackjack200/language/BaseLang.php
+++ src/blackjack200/language/BaseLang.php
@@ -1,9 +1,32 @@
 <?php
 
+declare(strict_types=1);
 
 namespace blackjack200\language;
 
-
 class BaseLang {
+	/** @var string[] */
+	protected array $map;
 
+	/** @param string[] $map */
+	public function __construct(array $map) {
+		$this->map = $map;
+	}
+
+	/** @param string[] $param */
+	public function translate(string $str, array $param = []) : string {
+		$fmt = $this->map[$str];
+		return self::replace($fmt, $param);
+	}
+
+	private static function replace(string $haystack, array $values) : string {
+		return str_replace(
+			array_map(
+				static fn(string $k) : string => sprintf('{%s}', $k),
+				array_keys($values)
+			),
+			array_values($values),
+			$haystack
+		);
+	}
 }
